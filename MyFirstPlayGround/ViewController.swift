c3e64c0a44c7e5512164cddac051b2ad36b9efdd
josman185hotm <josman185@gmail.com>
2016-08-20 16:16:53 -0400

    Initial Commit

---
+++ MyFirstPlayGround/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstPlayGround
+//
+//  Created by MCS on 8/20/16.
+//  Copyright © 2016 myOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
